abbc38077eabcdc75b98501df85f69248059f077
karla <karlacn@outlook.com>
2015-02-21 18:53:46 -0800

    Documentation and coding standards

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## 
 
-
-## Creates a CacheMatrix object in the form of a list with
-## accessors and mutators for the original and inverse matrix
+# Creates a CacheMatrix object in the form of a list with
+# accessors and mutators for the original and inverse matrix.  The calculation
+# is *NOT* performed here, this is nothing but a cache.  For information on
+# calculating the solution/inverse matrix refer to cacheSolve.
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -11,7 +11,9 @@ makeCacheMatrix <- function(x = matrix()) {
     }
     get <- function() x
 
-    setinverse <- function(inv) i <<- inv
+    setinverse <- function(inv){
+        i <<- inv
+    }
     getinverse <- function() i
     
     list(set = set, 
@@ -20,8 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
          getinverse = getinverse)
 }
 
-## Solves a system of equations explicitly created by the function 
-## makeCacheMatrix
+# Solves a system of equations explicitly created by the function 
+# makeCacheMatrix.  If the inverse/solution has been calculated before, it
+# will return the cached value, otherwise it will calculate the new solution
+# and set it using the functions available to the object 'x'.
 cacheSolve <- function(x, ...) {
     
     inv <-x $getinverse()
